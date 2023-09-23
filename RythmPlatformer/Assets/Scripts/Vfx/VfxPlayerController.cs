c245bb832ae5d9daa9a0e73da427d11f76d8c7a0
David Ruiz <deivid.ruiz93@gmail.com>
2023-09-23 00:23:54 -0300

    VFX particles

--- RythmPlatformer/Assets/Scripts/Vfx/VfxPlayerController.cs
+++ RythmPlatformer/Assets/Scripts/Vfx/VfxPlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class VfxPlayerController : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem _dustParticles;
+
     // Start is called before the first frame update
     void Start()
     {
